d0435dc7a242c4473846aaa44455053e0d00547c
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2025-06-17 12:25:33 -0300

    fix product query conections

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import prisma from "@/lib/prisma-client";
 import { Metadata } from 'next';
 import Image from 'next/image';
 import Navbar from "@/components/navbar";
@@ -27,23 +28,85 @@ export default async function ProductPage({ params }:{
     id: number
   }>
 }) {
+
+    async function getProductbyId(productId:number) {
+    try {
+       const product = await prisma.product.findUnique({
+            where:{ 
+                id:productId
+            },
+            select: {
+                name: true,
+                description: true,
+                price: true
+            }
+        })
+        // console.log(product)
+        return product
+    } catch (error) {
+        if (error instanceof Error){
+        console.log("Error: ", error.stack)
+        }
+    }
+    
+    }
+
+    async function getProductImagebyId(id:number) {
+      try {
+          const image = prisma.productImage.findMany({
+              where:{ 
+                  productId:id
+          }, select:{
+            imageUrl: true
+          }})
+          console.log(image)
+          return image
+      } catch (error) {
+          if (error instanceof Error){
+          console.log("Error: ", error.stack);
+      }
+
+      }
+    }
+
+    async function getProductReviewsbyId(id:number) {
+        try{
+            const review = prisma.productReview.findMany({
+                where:{ 
+                    productId:id
+                
+                },
+                take: 3
+            })
+            console.log(review)
+            return review
+          
+        } catch (error) {
+            if (error instanceof Error){
+             console.log("Error: ", error.stack);
+        }
+        }
+      }
     /*
     *Uncomment when database is ready
     */
-    // const productId = (await params).id;
-    // const product = await getProductbyId(productId);
-    // const productImage = await getProductImagebyId(productId);
-    // const productReviews = await getProductReviewsbyId(productId);
+    const productId = Number((await params).id);
+    const product = await getProductbyId(productId)
+   // const productImage = await getProductImagebyId(productId);
+    const productReviews = await getProductReviewsbyId(productId);
 
-    const product = productsData[0];
-    const productImage = productImagesData[0];
-    const productReviews = productReviewData[0];
+     //const product = productsData[0];
+     const productImage = productImagesData[0];
+     //const productReviews = productReviewData[0];
 
     if (!product) {
     notFound();
+    }  
+    if (!productReviews) {
+      return <div><p>There are no reviews about this product yet.</p></div>
     }
 
-    //console.log(product1)
+    console.log(productReviews)
 
     const handleAddToCart = (product: object, quantity: number) =>{
       const cart = localStorage.getItem("cart-section") || [];
@@ -62,14 +125,14 @@ export default async function ProductPage({ params }:{
             <Image
             className='thumbnails'
             src={productImage.imageUrl}
-            alt='thumbbail'
+            alt={`thumbbail image ${productImage.imageUrl}`}
             width={100}
             height={70}
             />
             <Image
                   className="product-img" 
                   src={productImage.imageUrl}
-                  alt="product Image"
+                  alt={`product Image ${productImage.imageUrl}`}
                   width={600}
                   height={400}
                   priority
@@ -80,8 +143,8 @@ export default async function ProductPage({ params }:{
             {/* Product description and details  */}
             <div className='product-details'>
               <h3>{product.name}</h3>
-              <p className='price'>${product.price}</p>
-              <div className='stars'>{<Star rate={productReviews.rating}/>}</div>
+              <p className='price'>${product.price.toString()}</p>
+              <div className='stars'>{<Star rate={4}/>}</div>
               <p>{product.description}</p>
               <p className='status'>In Stock</p>
              
@@ -101,7 +164,22 @@ export default async function ProductPage({ params }:{
           {/* //Reviews and comments */}
           <div className="reviews_container">
             <h3>Top Reviews of this product</h3>
-                {productReviewData.map((productReviews, index)=>{
+                {productReviews?.map((review)=>(
+                <div className='review' key={review.id}>
+                  <div className='user-info'>
+                    <User size={30}/> 
+                    <p>user {review.userId}</p>
+                    
+                  </div>
+                    <h4>{<Star rate={review.rating}/>} {review.title}</h4>
+                    <p className='date'>{review.createdAt.toDateString()}</p>
+                    <p className='review-text'>{review.reviewText}</p>
+                </div>
+                ))
+                }
+                <div><p>There are no reviews about this product yet.</p></div>
+    
+                {/* {productReviewData.map((productReviews, index)=>{
                   return (
                     <div className='review' key={index}>
                   <div className='user-info'>
@@ -114,12 +192,11 @@ export default async function ProductPage({ params }:{
                     <p className='review-text'>{productReviews.reviewText}</p>
                 </div>
                   )
-                })}
-                
+                })} */}
           </div>
           {/* Only if session is started. getSession() */}
           <div className="new-comments">
-            <NewReview productId={product.id}/>
+            <NewReview productId={productId}/>
           </div>
         </div>
       </main>
